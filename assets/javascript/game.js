b7e4f953478b5b878b76b76fa5c80989ede2774a
Claude Hatfield <r.hatfield269@gmail.com>
2019-09-06 20:36:35 -0500

    added letters array and set win condition

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,20 @@
-alert("Helllo");
+var wins = 0;
+var loss = 0;
+var guessLeft = 0;
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var computerChoice = letters[Math.floor(Math.random() * letters.length)];
+
+console.log(computerChoice);
+
+
+document.onkeyup = function(event){
+  var playerGuess = event.key;
+  var success =document.getElementById("wins");
+
+  if (playerGuess === computerChoice){
+    wins++
+    success.textContent = wins;
+  }
+}
