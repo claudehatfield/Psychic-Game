b7330bd3a608b58d617585d0d49cacef114ee68e
Claude Hatfield <r.hatfield269@gmail.com>
2019-09-07 20:53:11 -0500

    production code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,58 @@
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var wins = 0;
-var loss = 0;
-var userGuessLeft = 9;
-var userGuessedLetters;
-var correctLetter;
+var losses = 0;
+var left = 0;
+var letterGuessed;
 
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+reset();
+start();
 
-var computerChoice = letters[Math.floor(Math.random() * letters.length)];
-
-console.log(computerChoice);
-
-
-
-
-document.onkeyup = function (event) {
-  var playerGuess = event.key;
-  if (playerGuess === correctLetter) {
+document.onkeyup = function(event) {
+  var guess = event.key;
+  if (guess === rightLetter) {
     win();
-  } else if(userGuessLeft - 1 === 0){
-    lose();
-  }else {
-    fail(playerGuess);
+  } else if (left - 1 === 0) {
+    lost();
+  } else {
+    wrong(guess);
   }
+
   start();
 }
 
 
-function start() {
-  var startWins = document.getElementById("win");
-  var startLoss = document.getElementById("loss");
-  var startGuess = document.getElementById("left");
-  var startLetter = document.getElementById("soFar");
-  startWins.textContent = "#win " + wins;
-  startLoss.textContent = "#loss " + loss;
-  startGuess.textContent = "#left " + userGuessLeft;
-  startLetter.textContent = "#soFar" + userGuessedLetters.join(',');
+
+function start(){
+  var correct = document.getElementById("wins");
+  var lost = document.getElementById("losses");
+  var guessLeft = document.getElementById("guessLeft");
+  var guess = document.getElementById("guessed");
+
+  correct.textContent =  wins;
+  lost.textContent =  losses;
+  guessLeft.textContent = left;
+  guess.textContent = letterGuessed.join();
 }
 
-function win() {
-  wins++;
+function win(){
+  wins++
   reset();
 }
 
-function lost() {
-  loses++;
+function lost(){
+  losses++
   reset();
 }
 
-function fail(letter) {
-  userGuessLeft--;
-  userGuessedLetters.push(letter);
+function wrong(letter){
+  left--;
+  letterGuessed.push(letter);
+
 }
 
-function reset() {
-  useGuessLeft = 9;
-  userGuessedLetters = [];
-  correctLetter = letters[Math.floor(Math.random() * letters.length)];
-  console.log("Letter to guess: " + correctLetter);
+function reset(){
+  left = 9;
+  letterGuessed= [];
+  rightLetter = letters[Math.floor(Math.random() * letters.length)];
+  console.log("Letter to guess: " + rightLetter);
 }
